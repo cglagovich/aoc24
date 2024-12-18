6d74828061b7421131db5821c220f627351826f3
Colman Glagovich <cglagovich@gmail.com>
2024-12-18 11:13:45 -0500

    faster without pq

--- d18/solve.py
+++ d18/solve.py
@@ -1,5 +1,3 @@
-# part 1
-import heapq
 import sys
 n_bytes = int(sys.argv[2])
 grid_dim = int(sys.argv[3])
@@ -11,14 +9,13 @@ start_pos = 0 + 0j # r + c*j
 dirs = [1+0j, -1+0j, 0+1j, 0-1j]
 
 def search(walls):
-    visited = set()
-    visited.add(start_pos)
-    pq = []
-    heapq.heappush(pq, (0, start_pos.real, start_pos.imag))
+    visited = set(walls)
+    visited.add((start_pos.real, start_pos.imag))
+    pq = [(0, start_pos.real, start_pos.imag)]
     nsteps = 0
     while len(pq) > 0:
         nsteps += 1
-        cost, *cur_pos = heapq.heappop(pq)
+        cost, *cur_pos = pq.pop(0)
         cur_pos = complex(*cur_pos)
         if cur_pos == complex(grid_dim, grid_dim):
             return cost
@@ -26,10 +23,10 @@ def search(walls):
             npos = cur_pos + ndir
             if npos.real < 0 or npos.real > grid_dim or npos.imag < 0 or npos.imag > grid_dim:
                 continue
-            if npos not in visited and (npos.real, npos.imag) not in walls:
-                heapq.heappush(pq, (cost+1, npos.real, npos.imag))
+            if (npos.real, npos.imag) not in visited:
+                pq.append((cost+1, npos.real, npos.imag))
                 # Greedy?
-                visited.add(npos)
+                visited.add((npos.real, npos.imag))
     return False
 
 # part 1
@@ -37,7 +34,7 @@ print('part1', search(inp[:n_bytes]))
 
 # part 2
 # Do a binary search to find which byte closes the path
-import math
+# import math
 low = n_bytes
 high = len(open(fname).readlines())
 while low < high - 1:
