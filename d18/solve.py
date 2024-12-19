7d3dd2b901167bcc61c3fe7c3851d0e7dd56c69e
Colman Glagovich <cglagovich@gmail.com>
2024-12-19 08:00:11 -0500

    slight optimization d18

--- d18/solve.py
+++ d18/solve.py
@@ -7,26 +7,25 @@ inp = list(map(lambda line: tuple(map(lambda x: int(x), [l for l in line.strip()
 
 start_pos = 0 + 0j # r + c*j
 dirs = [1+0j, -1+0j, 0+1j, 0-1j]
+end_pos = complex(grid_dim, grid_dim)
 
 def search(walls):
     visited = set(walls)
     visited.add((start_pos.real, start_pos.imag))
-    pq = [(0, start_pos.real, start_pos.imag)]
-    nsteps = 0
-    while len(pq) > 0:
-        nsteps += 1
-        cost, *cur_pos = pq.pop(0)
-        cur_pos = complex(*cur_pos)
-        if cur_pos == complex(grid_dim, grid_dim):
+    q = [(0, start_pos)]
+    while len(q) > 0:
+        cost, cur_pos = q.pop(0)
+        if cur_pos == end_pos:
             return cost
         for ndir in dirs:
             npos = cur_pos + ndir
-            if npos.real < 0 or npos.real > grid_dim or npos.imag < 0 or npos.imag > grid_dim:
+            nposr, nposi = npos.real, npos.imag
+            if nposr < 0 or nposr > grid_dim or nposi < 0 or nposi > grid_dim:
                 continue
-            if (npos.real, npos.imag) not in visited:
-                pq.append((cost+1, npos.real, npos.imag))
+            if (nposr, nposi) not in visited:
+                q.append((cost+1, npos))
                 # Greedy?
-                visited.add((npos.real, npos.imag))
+                visited.add((nposr, nposi))
     return False
 
 # part 1
