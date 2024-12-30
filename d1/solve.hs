92ce7f6f0b3def6df22b2dac13ff27b1f01225cb
Colman Glagovich <cglagovich@gmail.com>
2024-12-30 17:49:37 -0500

    d1 haskell cleaner

--- d1/solve.hs
+++ d1/solve.hs
@@ -1,27 +1,27 @@
 import System.Environment (getArgs)
 import Data.List
 
-readInts :: [String] -> [Integer]
-readInts = map read
+-- trying to implement solve like Claude
+solvep1 :: String -> Integer
+solvep1 inp = sum $ zipWith (\a b -> abs(a - b)) sortedLeft sortedRight
+    where
+        (leftnums, rightnums) = unzip [(read l, read r) | line <- lines inp, let [l, r] = words line]
+        sortedLeft = sort leftnums
+        sortedRight = sort rightnums
 
-parseInp :: String -> [[Integer]]
-parseInp = map readInts . map words . lines
+solvep2 :: String -> Integer
+solvep2 inp = sum $ zipWith (*) lhs rhs_count
+    where
+        (lhs, rhs) = unzip [(read l, read r) | line <- lines inp, let [l, r] = words line]
+        rhs_count = [foldl (\acc x -> if x == needle then (acc+1) else acc) 0 rhs | needle <- lhs]
 
-sortLists :: [[Integer]] -> [[Integer]]
-sortLists = map sort . transpose
-
--- Returns a list of absolute differences
--- Transpose the list again and do the fold subtraction on the result
-diffLists :: [[Integer]] -> [Integer]
-diffLists lists = map (abs . foldl1 (-)) $ transpose lists
-
-
-solve :: String -> Integer
-solve = sum . diffLists . sortLists . parseInp
 
 main :: IO ()
 main = do
     args <- getArgs
     case args of
-        [filename] -> readFile filename >>= putStrLn . show . solve
+        [filename] -> do
+            inp <- readFile filename
+            putStrLn $ show $ solvep1 inp
+            putStrLn $ show $ solvep2 inp
         _ -> putStrLn "Usage: program filename"
